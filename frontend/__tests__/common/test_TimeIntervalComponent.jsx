d20b2c41aed281fdd884511d12b2d340a08ed9f6
Andy Gallagher <andy.gallagher@theguardian.com>
2018-11-05 17:42:14 +0000

    work on styling and breadcrumbs

--- frontend/__tests__/common/test_TimeIntervalComponent.jsx
+++ frontend/__tests__/common/test_TimeIntervalComponent.jsx
@@ -5,8 +5,33 @@ import sinon from 'sinon';
 import assert from 'assert';
 
 describe("TimeIntervalComponent", ()=> {
-    it("should display the time provided to it", () => {
+    it("should render seconds only", () => {
         const rendered = shallow(<TimeIntervalComponent editable={false} value={32}/>);
-        expect(rendered.find('span.duration').text()).toEqual("0:32");
+        expect(rendered.find('span.duration').text()).toEqual("32 seconds");
+    });
+
+    it("should render minutes and seconds", () => {
+        const rendered = shallow(<TimeIntervalComponent editable={false} value={125}/>);
+        expect(rendered.find('span.duration').text()).toEqual("2 minutes, 5 seconds");
+    });
+
+    it("should render hours, minutes and seconds", () => {
+        const rendered = shallow(<TimeIntervalComponent editable={false} value={3725}/>);
+        expect(rendered.find('span.duration').text()).toEqual("1 hour, 2 minutes, 5 seconds");
+    });
+
+    it("should render singular minutes", () => {
+        const rendered = shallow(<TimeIntervalComponent editable={false} value={65}/>);
+        expect(rendered.find('span.duration').text()).toEqual("1 minute, 5 seconds");
+    });
+
+    it("should render hours, and minutes only", () => {
+        const rendered = shallow(<TimeIntervalComponent editable={false} value={3660}/>);
+        expect(rendered.find('span.duration').text()).toEqual("1 hour, 1 minute");
+    });
+
+    it("should render hours only", () => {
+        const rendered = shallow(<TimeIntervalComponent editable={false} value={7200}/>);
+        expect(rendered.find('span.duration').text()).toEqual("2 hours");
     });
 });
--- frontend/app/common/TimeIntervalComponent.jsx
+++ frontend/app/common/TimeIntervalComponent.jsx
@@ -11,7 +11,13 @@ class TimeIntervalComponent extends React.Component {
 
     render(){
         const d = moment.duration(this.props.value,"seconds");
-        return <span className="duration">{d.format()}</span>
+        let fmtStringParts = [];
+        if(d.hours()>0) fmtStringParts = fmtStringParts.concat(["h [hours]"]);
+        if(d.minutes()>0) fmtStringParts = fmtStringParts.concat(["m [minutes]"]);
+        if(d.seconds()>0) fmtStringParts = fmtStringParts.concat(["s [seconds]"]);
+        const formatString = fmtStringParts.join(", ");
+
+        return <span className="duration">{d.format(formatString)}</span>
     }
 }
 
