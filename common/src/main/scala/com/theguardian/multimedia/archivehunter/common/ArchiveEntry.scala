8608d2f75d74903120607a9dfa1e67fca8178a15
Andy Gallagher <andy.gallagher@theguardian.com>
2018-11-09 17:38:34 +0000

    adding a scanner for legacy proxy buckets and endpoint to trigger it

--- common/src/main/scala/com/theguardian/multimedia/archivehunter/common/ArchiveEntry.scala
+++ common/src/main/scala/com/theguardian/multimedia/archivehunter/common/ArchiveEntry.scala
@@ -17,7 +17,7 @@ import java.util.Base64
 
 import io.circe.generic.semiauto._
 
-object ArchiveEntry extends ((String, String, String, Option[String], Long, ZonedDateTime, String, MimeType, Boolean, StorageClass, Boolean)=>ArchiveEntry){
+object ArchiveEntry extends ((String, String, String, Option[String], Long, ZonedDateTime, String, MimeType, Boolean, StorageClass, Boolean)=>ArchiveEntry) with DocId {
   private val logger = LogManager.getLogger(getClass)
 
   def getFileExtension(str: String):Option[String] = {
@@ -26,28 +26,6 @@ object ArchiveEntry extends ((String, String, String, Option[String], Long, Zone
       Some(tokens(1))
     } else {
       None
-    }
-  }
-
-  /**
-    * Calculates an ID unique to this bucket/path combination, that is not longer than the elasticsearch limit
-    * @param bucket bucket that the file is coming from
-    * @param key path to the file in `bucket`
-    */
-  def makeDocId(bucket: String, key:String):String = {
-    val maxIdLength=512
-    val encoder = java.util.Base64.getEncoder
-
-    val initialString = bucket + ":" + key
-    if(initialString.length<=maxIdLength){
-      encoder.encodeToString(initialString.toCharArray.map(_.toByte))
-    } else {
-      /* I figure that the best way to get something that should be unique for a long path is to chop out the middle */
-      val chunkLength = initialString.length/3
-      val stringParts = initialString.grouped(chunkLength).toList
-      val midSectionLength = maxIdLength - chunkLength*2  //FIXME: what if chunkLength*2>512??
-      val finalString = stringParts.head + stringParts(1).substring(0, midSectionLength) + stringParts(2)
-      encoder.encodeToString(finalString.toCharArray.map(_.toByte))
     }
   }
 
