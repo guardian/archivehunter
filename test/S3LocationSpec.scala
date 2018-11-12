014f19368b333f1b12dffb70365d3c6e61ef29f9
Andy Gallagher <andy.gallagher@theguardian.com>
2018-11-12 21:05:14 +0000

    AWS signer working according to their examples for GET requests (no body)

--- test/S3LocationSpec.scala
+++ test/S3LocationSpec.scala
@@ -13,16 +13,18 @@ import play.api.Logger
 
 import scala.concurrent.{Await, ExecutionContext}
 import scala.concurrent.duration._
+import scala.collection.immutable.Seq
 
 /* A tiny class that can be used as a Specs2 ‘context’. */
 abstract class AkkaTestkitSpecs2Support extends TestKit(ActorSystem())
   with After
   with ImplicitSender {
   // make sure we shut down the actor system after all tests have run
-  def after = system.terminate()
+  def after = Await.result(system.terminate(), 30 seconds)
 }
 
 class S3LocationSpec extends Specification {
+  sequential
   class TestClass(loggerval:Logger, m:Materializer, ecval:ExecutionContext) extends S3Signer {
     override implicit val mat:Materializer = m
     override protected val logger=loggerval
@@ -31,22 +33,13 @@ class S3LocationSpec extends Specification {
 
   "S3Signer" should {
     "sign a sample GET request correctly" in new AkkaTestkitSpecs2Support {
-      import scala.collection.immutable.Seq
+      /* example taken from https://docs.aws.amazon.com/AmazonS3/latest/API/sig-v4-header-based-auth.html */
       val logger=Logger(getClass)
       val mat = ActorMaterializer()
       implicit val ec = system.dispatcher
       val test = new TestClass(logger, mat, ec)
 
       val credentialsProvider = new AWSStaticCredentialsProvider(new BasicAWSCredentials("AKIAIOSFODNN7EXAMPLE", "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"))
-//      val testInput = new HttpRequest(
-//        "GET",
-//        "https://examplebucket.s3.amazonaws.com/test.txt",
-//        Seq(
-//          HttpHeader("Range", "bytes=0-9")
-//        ),
-//        HttpEntity.NoEntity,
-//        HttpProtocol.HTTP_1_1
-//      )
 
       val headers = Seq(HttpHeader.parse("Range", "bytes=0-9"),HttpHeader.parse("Host", "examplebucket.s3.amazonaws.com")).map({
         case Ok(result, errors)=>result
@@ -63,6 +56,31 @@ class S3LocationSpec extends Specification {
 
       val headerMap = result.headers.map(hdr=>Tuple2(hdr.name(), hdr.value())).toMap
       headerMap("Authorization") mustEqual "AWS4-HMAC-SHA256 Credential=AKIAIOSFODNN7EXAMPLE/20130524/us-east-1/s3/aws4_request,SignedHeaders=host;range;x-amz-content-sha256;x-amz-date,Signature=f0e8bdb87c964420e857bd35b5d6ed310bd44f0170aba48dd91039c6036bdb41"
+    }
+
+    "sign a GET request with parameters correctly" in new AkkaTestkitSpecs2Support {
+      val logger=Logger(getClass)
+      val mat = ActorMaterializer()
+      implicit val ec = system.dispatcher
+      val test = new TestClass(logger, mat, ec)
+
+      val credentialsProvider = new AWSStaticCredentialsProvider(new BasicAWSCredentials("AKIAIOSFODNN7EXAMPLE", "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"))
+
+      val headers = Seq(HttpHeader.parse("Host", "examplebucket.s3.amazonaws.com")).map({
+        case Ok(result, errors)=>result
+        case Error(errs)=>throw new RuntimeException(errs.toString)
+      })
+
+      val testInput = HttpRequest(HttpMethod.custom("GET"),
+        Uri("https://examplebucket.s3.amazonaws.com/?lifecycle"),
+        headers
+      )
+
+      val fakeTime = OffsetDateTime.of(2013, 5, 24, 0,0,0,0,ZoneOffset.UTC)
+      val result = Await.result(test.signHttpRequest(testInput,Region.getRegion(Regions.US_EAST_1),"s3", credentialsProvider, Some(fakeTime)), 10 seconds)
+
+      val headerMap = result.headers.map(hdr=>Tuple2(hdr.name(), hdr.value())).toMap
+      headerMap("Authorization") mustEqual "AWS4-HMAC-SHA256 Credential=AKIAIOSFODNN7EXAMPLE/20130524/us-east-1/s3/aws4_request,SignedHeaders=host;x-amz-content-sha256;x-amz-date,Signature=fea454ca298b7da1c68078a5d1bdbfbbe0d65c699e0f91ac7a200a0136783543"
     }
   }
 }
