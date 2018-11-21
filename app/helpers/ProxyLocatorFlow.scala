3cb73458c075026f28e4bc8cf4cdeafd88babf86
Andy Gallagher <andy.gallagher@theguardian.com>
2018-11-21 14:56:10 +0000

    bulk resolution of proxies using the new (accurate) method.

---
+++ app/helpers/ProxyLocatorFlow.scala
@@ -0,0 +1,50 @@
+package helpers
+
+import akka.stream.{Attributes, FlowShape, Inlet, Outlet}
+import akka.stream.stage.{AbstractInHandler, AbstractOutHandler, GraphStage, GraphStageLogic}
+import com.theguardian.multimedia.archivehunter.common.{ArchiveEntry, ProxyLocation}
+import javax.inject.Inject
+import models.ScanTargetDAO
+import play.api.{Configuration, Logger}
+
+import scala.concurrent.Await
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+
+class ProxyLocatorFlow @Inject() (playConfig:Configuration, s3ClientManager: S3ClientManager)(implicit scanTargetDAO:ScanTargetDAO) extends GraphStage[FlowShape[ArchiveEntry, ProxyLocation]]{
+  private final val in:Inlet[ArchiveEntry] = Inlet.create("ProxyLocatorFlow.in")
+  private final val out:Outlet[ProxyLocation] = Outlet.create("ProxyLocatorFlow.out")
+  private val logger = Logger(getClass)
+
+  override def shape: FlowShape[ArchiveEntry, ProxyLocation] = FlowShape.of(in,out)
+
+  val awsProfile = playConfig.getOptional[String]("externalData.awsProfile")
+
+  override def createLogic(inheritedAttributes: Attributes): GraphStageLogic = new GraphStageLogic(shape) {
+
+    private implicit val s3Client = s3ClientManager.getS3Client(awsProfile)
+    setHandler(in, new AbstractInHandler {
+      override def onPush(): Unit = {
+        val elem = grab(in)
+        logger.debug(s"Got archive entry $elem")
+
+        val potentialProxyLocations = Await.result(ProxyLocator.findProxyLocation(elem), 10 seconds)
+        logger.debug(s"Got $potentialProxyLocations for $elem")
+        if(potentialProxyLocations.isEmpty){
+          logger.info(s"Could not find any potential proxies for ${elem.bucket}/${elem.path}")
+          pull(in)
+        } else {
+          if(potentialProxyLocations.length>1) logger.info(s"Found ${potentialProxyLocations.length} potential proxies for ${elem.bucket}/${elem.path}, going with the first")
+          logger.debug(s"Outputting ${potentialProxyLocations.head}")
+          push(out, potentialProxyLocations.head)
+        }
+      }
+    })
+
+    setHandler(out, new AbstractOutHandler {
+      override def onPull(): Unit = {
+        pull(in)
+      }
+    })
+  }
+}
--- app/services/LegacyProxiesScanner.scala
+++ app/services/LegacyProxiesScanner.scala
@@ -1,9 +1,9 @@
 package services
 
 import akka.actor.{Actor, ActorSystem, Cancellable}
-import akka.stream.scaladsl.Keep
+import akka.stream.scaladsl.{Keep, Source}
 import akka.stream.{ActorMaterializer, KillSwitches}
-import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput
+import com.amazonaws.services.dynamodbv2.model._
 import com.google.inject.Injector
 import com.theguardian.multimedia.archivehunter.common.ProxyLocation
 import helpers._
@@ -11,7 +11,7 @@ import javax.inject.Inject
 import models.{ScanTarget, ScanTargetDAO}
 import play.api.{Configuration, Logger}
 
-import scala.concurrent.{ExecutionContext, Promise}
+import scala.concurrent.{ExecutionContext, Future, Promise}
 import scala.concurrent.duration._
 import scala.util.{Failure, Success}
 
@@ -29,8 +29,13 @@ object LegacyProxiesScanner {
 class LegacyProxiesScanner @Inject()(config:Configuration, ddbClientMgr:DynamoClientManager, s3ClientMgr:S3ClientManager,
                                      esClientMgr:ESClientManager, scanTargetDAO: ScanTargetDAO, injector:Injector)(implicit system:ActorSystem)extends Actor {
   import LegacyProxiesScanner._
+  import com.sksamuel.elastic4s.streams.ReactiveElastic._
+  import com.sksamuel.elastic4s.http.ElasticDsl._
+
   private val logger = Logger(getClass)
   implicit val mat = ActorMaterializer.create(system)
+  val indexName = config.getOptional[String]("elasticsearch.index").getOrElse("archivehunter")
+  val tableName = config.get[String]("proxies.tableName")
 
   implicit val ec:ExecutionContext = system.dispatcher
 
@@ -38,37 +43,70 @@ class LegacyProxiesScanner @Inject()(config:Configuration, ddbClientMgr:DynamoCl
 
   private var tableReadyTimer:Option[Cancellable] = None
 
+  val updatingCapacityTarget = 300
+
   /**
     * initiates an update to the table's provisioned capacity.
     * @param boostTo provisioned write capacity to boost to
     * @param tgt [[ScanTarget]]
-    * @return a Boolean indicating whether you need to wait for the table to be ready.  If false, then proceed immediately.
-    *         If true then return; [[CheckTableReady]] will be dispatched at regular intervals to check whether the table is ready.
+    * @return a Boolean indicating whether you need to wait for the table to be ready.  If true, then proceed immediately.
+    *         If false then return; [[CheckTableReady]] will be dispatched at regular intervals to check whether the table is ready.
     *         Once it is then ScanBucket will be dispatched again with the [[ScanTarget]] provided in the `tgt` argument
     */
   def updateProvisionedWriteCapacity(boostTo: Int, tgt:ScanTarget, completionPromise:Option[Promise[Boolean]]):Either[LPSError, Boolean] = {
-    val tableName = config.get[String]("proxies.tableName")
     val result = ddbClient.describeTable(config.get[String]("proxies.tableName"))
 
     if(result.getTable.getTableStatus!="ACTIVE"){
       logger.warn(s"Can't update table status while it is in ${result.getTable.getTableStatus} state.")
       Left(WrongTableState)
     } else {
-      val tp = result.getTable.getProvisionedThroughput
-      if (tp.getWriteCapacityUnits == boostTo) {
-        Right(false)
+      val tableThroughput = result.getTable.getProvisionedThroughput
+      val indexName = result.getTable.getGlobalSecondaryIndexes.get(0).getIndexName
+
+      val indexThroughput = result.getTable.getGlobalSecondaryIndexes.get(0).getProvisionedThroughput
+      logger.info(s"index name is $indexName, throughput is $indexThroughput")
+
+      if (tableThroughput.getWriteCapacityUnits == boostTo && indexThroughput.getWriteCapacityUnits==boostTo) {
+        Right(true)
       } else {
         val msgToSend = completionPromise match {
           case Some(promise) => CheckTableReadyPrms(promise)
           case None => CheckTableReady(ScanBucket(tgt))
         }
         try {
-          ddbClient.updateTable(config.get[String]("proxies.tableName"),
-            new ProvisionedThroughput()
-              .withReadCapacityUnits(tp.getReadCapacityUnits)
-              .withWriteCapacityUnits(boostTo.toLong))
+          val newTableThroughput = new ProvisionedThroughput()
+            .withWriteCapacityUnits(boostTo.toLong)
+            .withReadCapacityUnits(tableThroughput.getReadCapacityUnits)
+          val newIndexThroughput = new ProvisionedThroughput()
+            .withWriteCapacityUnits(boostTo.toLong)
+            .withReadCapacityUnits(10L)
+
+          val initialRq = new UpdateTableRequest()
+            .withTableName(tableName)
+
+          val tableRq = if(tableThroughput.getWriteCapacityUnits == boostTo){
+            initialRq
+          } else {
+            initialRq.withProvisionedThroughput(newTableThroughput)
+          }
+
+            val indexRq = if(indexThroughput.getWriteCapacityUnits==boostTo){
+              tableRq
+            } else {
+              tableRq.withGlobalSecondaryIndexUpdates(new GlobalSecondaryIndexUpdate()
+                .withUpdate(new UpdateGlobalSecondaryIndexAction()
+                  .withIndexName(indexName)
+                  .withProvisionedThroughput(newIndexThroughput)))
+            }
+
+
+          ddbClient.updateTable(indexRq) //this raises if it fails, caught just below.
+//          ddbClient.updateTable(config.get[String]("proxies.tableName"),
+//            new ProvisionedThroughput()
+//              .withReadCapacityUnits(tp.getReadCapacityUnits)
+//              .withWriteCapacityUnits(boostTo.toLong))
           tableReadyTimer = Some(system.scheduler.schedule(10 seconds, 1 second, self, msgToSend))
-          Right(true)
+          Right(false)
         } catch {
           case ex:Throwable=>Left(ProviderError(ex))
         }
@@ -104,10 +142,10 @@ class LegacyProxiesScanner @Inject()(config:Configuration, ddbClientMgr:DynamoCl
 
   override def receive: Receive = {
     case ScanBucket(tgt)=>
-      logger.info("Boosting provisioned write capacity to 150...")
+      logger.info(s"Boosting provisioned write capacity to $updatingCapacityTarget...")
       //updateProvisionedWriteCapacity returns False if it needs to update capacity.  We will get re-dispatched by the scheduler in
       //that case
-      val canStart = updateProvisionedWriteCapacity(150,tgt,None) match {
+      val canStart = updateProvisionedWriteCapacity(updatingCapacityTarget,tgt,None) match {
         case Left(WrongTableState)=>
           logger.error("Table is not in the right state, can't start a scan.")
           false
@@ -116,18 +154,31 @@ class LegacyProxiesScanner @Inject()(config:Configuration, ddbClientMgr:DynamoCl
           false
         case Right(flag)=>flag
       }
+      logger.debug(s"canStart: $canStart")
 
       if(canStart) {
         logger.info(s"Starting proxy scan on $tgt")
-        val client = s3ClientMgr.getAlpakkaS3Client(config.getOptional[String]("externalData.awsProfile"))
-
-        val keySource = client.listBucket(tgt.proxyBucket, None)
+        val esClient = esClientMgr.getClient()
+//        val client = s3ClientMgr.getAlpakkaS3Client(config.getOptional[String]("externalData.awsProfile"))
+//
+//        val keySource = client.listBucket(tgt.proxyBucket, None)
+        val searchHitPublisher = esClient.publisher(search(indexName) matchQuery ("bucket.keyword", tgt.bucketName) scroll "1m")
+        val searchHitSource = Source.fromPublisher(searchHitPublisher)
+        val archiveEntryConverter = new SearchHitToArchiveEntryFlow
+        val proxyLocator = injector.getInstance(classOf[ProxyLocatorFlow])
         val ddbSink = injector.getInstance(classOf[DDBSink])
         val streamCompletionPromise = Promise[Unit]()
         val eosDetect = new EOSDetect[Unit, ProxyLocation](streamCompletionPromise, ())
         val converter = new S3ToProxyLocationFlow(s3ClientMgr, config, tgt.bucketName, Seq())
 
-        keySource.via(converter).log("legacy-proxies-scanner").via(eosDetect).to(ddbSink).run()
+        //keySource.via(converter).log("legacy-proxies-scanner").via(eosDetect).to(ddbSink).run()
+        searchHitSource
+          .via(archiveEntryConverter)
+          .via(proxyLocator)
+          .via(eosDetect)
+          .log("proxies-scanner")
+          .to(ddbSink)
+          .run()
 
         streamCompletionPromise.future.onComplete({
           case Success(_)=>
@@ -146,11 +197,13 @@ class LegacyProxiesScanner @Inject()(config:Configuration, ddbClientMgr:DynamoCl
       }
 
     case CheckTableReady(nextMsg:LPSMessage)=>
+      logger.debug("checkTableReady")
       if(isTableReady){
         logger.info(s"Re-sending $nextMsg")
         self ! nextMsg
       }
     case CheckTableReadyPrms(completionPromise)=>
+      logger.debug("checkTableReadyPrms")
       if(isTableReady){
         completionPromise.complete(Success(true))
       }
