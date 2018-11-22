d1f7dd367e9d3e30a6f70680772d192519884dbc
Andy Gallagher <andy.gallagher@theguardian.com>
2018-11-22 12:33:54 +0000

    push to an ecs repo

--- dockerProxyImage/build_and_push_dockerimage.sh
+++ dockerProxyImage/build_and_push_dockerimage.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
 cd "${BASH_SOURCE%/*}/" || exit                            # cd into the bundle and use relative paths
-docker build . -t guardianmultimedia/archivehunter-proxyimage:$BUILD_NUM
-docker push guardianmultimedia/archivehunter-proxyimage:$BUILD_NUM
+
+#need to setup up AWS credentials in the build config
+declare -x LOGIN_CMD=$(aws ecr get-login --no-include-email --region eu-west-1)
+${LOGIN_CMD}
+
+if [ "${DOCKER_PATH}" == "" ]; then
+    declare -x DOCKER_PATH=guardianmultimedia/archivehunter-proxyimage
+fi
+
+docker build . -t ${DOCKER_PATH}:$BUILD_NUM
+docker push ${DOCKER_PATH}:$BUILD_NUM
