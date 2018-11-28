6c62b5f693d94b61081000d1164dc15396e23a5d
Andy Gallagher <andy.gallagher@theguardian.com>
2018-11-28 07:28:17 +0000

    working audio waveform generation for thumbnail

---
+++ dockerProxyImage/scripts/extract_thumbnail.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+#expects arguments:  extract_thumbnail.sh {s3-uri-of-source} {s3-bucket-for-proxies} {http-uri-archivehunter}
+
+if [ "$1" == "" ]; then
+    echo "You must specific a source S3 URI"
+    exit 1
+fi
+
+if [ "$2" == "" ]; then
+    echo "You must specify a destination bucket"
+    exit 1
+fi
+
+if [ "$3" == "" ]; then
+    echo "You must specify a callback URL"
+    exit 1
+fi
+
+echo Downloading source media $1...
+aws s3 cp "$1" /tmp/mediafile
+if [ "$?" != "0" ]; then
+    echo Could not download source media.
+    exit 1
+fi
+
+MIMETYPE=$(file -b --mime-type /tmp/mediafile)
+
+if [[ "$MIMETYPE" =~ ^audio.* ]]; then
+    echo Got audio file
+    mv /tmp/mediafile /tmp/audiofile
+    extract_audio_thumbnail.sh $1 $2 $3
+    exit $?
+elif [[ "$MIMETYPE" =~ ^video.* ]]; then
+    echo Got video file
+    mv /tmp/mediafile /tmp/videofile
+    extract_video_thumbnail.sh $1 $2 $3
+    exit $?
+else
+    echo ${MIMETYPE} files are not supported yet
+    exit 1
+fi
