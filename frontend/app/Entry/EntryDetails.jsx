7358f5b40606022339659b45d10ffcf35752f4d9
Andy Gallagher <andy.gallagher@theguardian.com>
2018-12-19 12:34:24 +0000

    add overflow scroll to entry details

--- frontend/app/Entry/EntryDetails.jsx
+++ frontend/app/Entry/EntryDetails.jsx
@@ -109,7 +109,7 @@ class EntryDetails extends React.Component {
         }
         const fileinfo = this.extractFileInfo(this.props.entry.path);
 
-        return <div className="entry-details">
+        return <div className="entry-details" style={{overflow: "scroll"}}>
                 <EntryPreview entryId={this.props.entry.id}
                               hasProxy={this.props.entry.proxied}
                               fileExtension={this.props.entry.file_extension}
