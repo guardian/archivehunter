5c1b16cca02c69fb1dd20b12ba526500c7f5f7e0
Andy Gallagher <andy.gallagher@theguardian.com>
2019-02-07 18:22:40 +0000

    JS test fix

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,10 +1,11 @@
-{
+module.exports = {
   "presets": [
     "@babel/env",
     "@babel/react"
   ],
   "plugins": [
     "@babel/plugin-proposal-function-bind",
-    "@babel/plugin-proposal-class-properties"
+    "@babel/plugin-proposal-class-properties",
+    "@babel/plugin-transform-react-jsx"
   ]
-}
+};
