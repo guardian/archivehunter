0fd512e5333be34744613b5f3373947a84f3cbce
andy_gallagher <andy.gallagher@theguardian.com>
2022-03-21 10:23:29 +0000

    fix ClockPerInstance, logging tweaks

--- app/services/ClockPerInstance.scala
+++ app/services/ClockPerInstance.scala
@@ -2,18 +2,20 @@ package services
 
 import akka.actor.{Actor, ActorRef, Timers}
 import com.theguardian.multimedia.archivehunter.common.ExtValueConverters
-import play.api.Logger
+import org.slf4j.LoggerFactory
 
-import javax.inject.Named
+import javax.inject.{Inject, Named}
 import scala.concurrent.duration._
 
-class ClockPerInstance(
+class ClockPerInstance @Inject() (
                         @Named("ingestProxyQueue") ingestProxyQueue: ActorRef,
                         @Named("proxyFrameworkQueue") proxyFrameworkQueue:ActorRef,
                         @Named("fileMoveQueue") fileMoveQueue:ActorRef,
                       ) extends Actor with Timers with ExtValueConverters {
   import ClockSingleton._
+  private val logger = LoggerFactory.getLogger(getClass)
 
+  logger.info("Starting ClockPerInstance")
   timers.startTimerAtFixedRate(RapidClockTick, RapidClockTick, 30.seconds)
 
   override def receive: Receive = {
